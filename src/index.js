7d21d19f99fa3d698314d47df5b8d7994aef0067
Sam Gaus <sam@gaus.co.uk>
2017-06-15 16:10:21 +0100

    Add eslint and fix

--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
-import tingle from 'tingle.js';
-import { iframeResizer } from 'iframe-resizer';
-import './style.css';
+import tingle from "tingle.js";
+import { iframeResizer } from "iframe-resizer";
+import "./style.css";
 
 export default function generateModal(url) {
   // Generate id for this modal
   const id = `widget-${Date.now()}`;
 
   // Create modal
-  const modal = new tingle.modal({
+  const Modal = tingle.modal;
+  const modal = new Modal({
     closeMethods: [],
-    cssClass: ['mtk-widget'],
+    cssClass: ["mtk-widget"]
   });
-  modal.setContent(`<iframe id="${id}" src="${url}" style="width: 100%"></iframe>`);
+  modal.setContent(
+    `<iframe id="${id}" src="${url}" style="width: 100%"></iframe>`
+  );
 
   // Set up iframe resizer
-  iframeResizer({
-    minHeight: 200,
-    checkOrigin: false,
-    heightCalculationMethod: 'taggedElement',
-    resizeFrom: 'child',
-    resizedCallback: () => {
-      modal.checkOverflow();
-    },
-    messageCallback: ({ message }) => {
-      if (message === 'MTK_SB_CLOSE') {
-        modal.close();
+  iframeResizer(
+    {
+      minHeight: 200,
+      checkOrigin: false,
+      heightCalculationMethod: "taggedElement",
+      resizeFrom: "child",
+      resizedCallback: () => {
+        modal.checkOverflow();
+      },
+      messageCallback: ({ message }) => {
+        if (message === "MTK_SB_CLOSE") {
+          modal.close();
+        }
       }
     },
-  }, `#${id}`);
+    `#${id}`
+  );
 
   return modal;
 }
--- src/widget.js
+++ src/widget.js
@@ -1,17 +1,19 @@
-import generateModal from './index';
+/* eslint-env browser */
+
+import generateModal from "./index";
 
 // Find script
 const script = document.currentScript;
 
 // Extract parameters
-const url = script.getAttribute('data-url') || '';
-const buttonLabel = script.getAttribute('data-label') || 'Open widget';
+const url = script.getAttribute("data-url") || "";
+const buttonLabel = script.getAttribute("data-label") || "Open widget";
 
 // Generate modal
 const modal = generateModal(url);
 
 // Create button
-const button = document.createElement('button');
+const button = document.createElement("button");
 button.innerText = buttonLabel;
 button.onclick = () => {
   modal.open();
